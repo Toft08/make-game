0859b7067543fe582af11f580f64e5694791cb87
Roope Hongisto <roope@Roopes-MacBook-Air.local>
2025-03-06 17:00:45 +0200

    lines displays now correctly

--- game.js
+++ game.js
@@ -400,8 +400,8 @@ document.addEventListener("keydown", (event) => {
 
 
 function updateScoreboard() {
-    document.getElementById("score").textContent = totalClearedRows;
-    document.getElementById("final-score").textContent = totalClearedRows;
+    document.getElementById("lines").textContent = totalClearedRows;
+    document.getElementById("final-lines").textContent = totalClearedRows;
 
 }
 
